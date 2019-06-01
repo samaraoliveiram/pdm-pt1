acc39a13819c0881b476a3a8db0ed64fb445d384
Samara Motta <samaramotta1998@gmail.com>
2019-06-01 02:57:16 -0300

    Configure routes and ceate class movie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: "", loadChildren: "./tabs/tabs.module#TabsPageModule" }
+  // { path: "details/:id", loadChildren: "./tab2/tab2.module.ts" },
 ];
 @NgModule({
   imports: [
